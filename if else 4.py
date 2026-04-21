280c98059df4064a9d92501c4d82465c404e65a0
Kavin <kavinraja4613@gmail.com>
2026-04-21 13:57:26 +0530

    Create if else 4.py

---
+++ if else 4.py
@@ -0,0 +1,6 @@
+attendance=int(input("Enter the attendance percentage: "))
+marks=int(input("Enter the marks obtained: "))
+if marks>=50 and attendance>=70:
+    print("You are eligible for the exam.")
+else:
+    print("You are not eligible for the exam.")
